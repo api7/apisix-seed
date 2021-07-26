377b60d5478d91af8fff2879c1c6d5bf8520e1e3
gu18168 <gu18168@gmail.com>
2021-07-26 14:03:28 +0800

    feat: add discoverer

---
+++ internal/discoverer/discoverer.go
@@ -0,0 +1,87 @@
+package discoverer
+
+import (
+	"errors"
+	"fmt"
+	"github.com/api7/apisix-seed/internal/utils"
+	"strconv"
+)
+
+var (
+	queryChecks = [3]string{"event", "entity", "service"}
+	Discoveries = make(map[string]Discover)
+)
+
+type Discover func(disConfig interface{}) Discoverer
+
+// Discoverer defines the component that interact nacos, consul and so on
+type Discoverer interface {
+	Stop()
+	Query(utils.Message) error
+	Watch() chan utils.Message
+}
+
+type Node struct {
+	host   string
+	weight int
+}
+
+type Service struct {
+	name     string
+	nodes    []Node
+	entities []string
+	args     map[string]string
+}
+
+// queryDecode check and extract values from the query message
+func queryDecode(msg utils.Message) ([]string, map[string]string, error) {
+	msgLength := len(msg)
+	if msgLength < 3 {
+		return nil, nil, errors.New("incorrect query message format")
+	}
+
+	// Check the required parts of the query message
+	msgValues := make([]string, 3)
+	for i := 0; i < 3; i++ {
+		if msg[i].Key != queryChecks[i] {
+			err := fmt.Sprintf("incorrect query part %d: give %s, require %s", i+1, msg[i].Key, queryChecks[i])
+			return nil, nil, errors.New(err)
+		}
+		msgValues[i] = msg[i].Value
+	}
+
+	// Check query event
+	switch event := msgValues[0]; event {
+	case utils.EventAdd, utils.EventUpdate, utils.EventDelete:
+	default:
+		err := fmt.Sprintf("incorrect query event: %s", event)
+		return nil, nil, errors.New(err)
+	}
+
+	// If additional service discovery arguments exist
+	var msgArgs map[string]string
+	if msgLength > 3 {
+		msgArgs = make(map[string]string, msgLength-3)
+		for i := 3; i < msgLength; i++ {
+			msgArgs[msg[i].Key] = msg[i].Value
+		}
+	}
+
+	return msgValues, msgArgs, nil
+}
+
+// watchEncode encodes a service to the watch message
+func watchEncode(service Service) utils.Message {
+	msg := make(utils.Message, 0, 2+len(service.entities)+2*len(service.nodes))
+	msg.Add("event", utils.EventUpdate)
+	msg.Add("service", service.name)
+	for _, entity := range service.entities {
+		msg.Add("entity", entity)
+	}
+	for _, node := range service.nodes {
+		msg.Add("node", node.host)
+		msg.Add("weight", strconv.Itoa(node.weight))
+	}
+
+	return msg
+}
---
+++ internal/discoverer/discoverer_test.go
@@ -0,0 +1,149 @@
+package discoverer
+
+import (
+	"fmt"
+	"github.com/api7/apisix-seed/internal/utils"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestQueryCheck(t *testing.T) {
+	tests := []struct {
+		caseDesc string
+		giveMsg  [][2]string
+		wantErr  error
+	}{
+		{
+			caseDesc: "Test Wrong Format: empty message",
+			giveMsg:  [][2]string{},
+			wantErr:  fmt.Errorf("incorrect query message format"),
+		},
+		{
+			caseDesc: "Test Incorrect Part: incorrect event",
+			giveMsg: [][2]string{
+				{"action", "add"},
+				{"entity", "upstream;1"},
+				{"service", "test"},
+			},
+			wantErr: fmt.Errorf("incorrect query part 1: give action, require event"),
+		},
+		{
+			caseDesc: "Test Incorrect Part: incorrect entity",
+			giveMsg: [][2]string{
+				{"event", "add"},
+				{"entities", "upstream;1"},
+				{"service", "test"},
+			},
+			wantErr: fmt.Errorf("incorrect query part 2: give entities, require entity"),
+		},
+		{
+			caseDesc: "Test Incorrect Part: incorrect service",
+			giveMsg: [][2]string{
+				{"event", "add"},
+				{"entity", "upstream;1"},
+				{"services", "test"},
+			},
+			wantErr: fmt.Errorf("incorrect query part 3: give services, require service"),
+		},
+		{
+			caseDesc: "Test Incorrect Event",
+			giveMsg: [][2]string{
+				{"event", "remove"},
+				{"entity", "upstream;1"},
+				{"service", "test"},
+			},
+			wantErr: fmt.Errorf("incorrect query event: remove"),
+		},
+	}
+
+	for _, tc := range tests {
+		msg := make(utils.Message, 0, len(tc.giveMsg))
+		for _, pair := range tc.giveMsg {
+			msg.Add(pair[0], pair[1])
+		}
+		_, _, err := queryDecode(msg)
+		assert.True(t, tc.wantErr.Error() == err.Error(), tc.caseDesc)
+	}
+}
+
+func TestQueryEncode(t *testing.T) {
+	tests := []struct {
+		caseDesc   string
+		giveMsg    [][2]string
+		wantValues []string
+		wantArgs   map[string]string
+	}{
+		{
+			caseDesc: "Test Query Encode without extra arguments",
+			giveMsg: [][2]string{
+				{"event", "add"},
+				{"entity", "upstream;1"},
+				{"service", "test"},
+			},
+			wantValues: []string{"add", "upstream;1", "test"},
+			wantArgs:   nil,
+		},
+		{
+			caseDesc: "Test Query Encode with arguments",
+			giveMsg: [][2]string{
+				{"event", "add"},
+				{"entity", "upstream;1"},
+				{"service", "test"},
+				{"namespace_id", "test_ns"},
+			},
+			wantValues: []string{"add", "upstream;1", "test"},
+			wantArgs: map[string]string{
+				"namespace_id": "test_ns",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		msg := make(utils.Message, 0, len(tc.giveMsg))
+		for _, pair := range tc.giveMsg {
+			msg.Add(pair[0], pair[1])
+		}
+		values, args, err := queryDecode(msg)
+		assert.Nil(t, err)
+		assert.True(t, reflect.DeepEqual(values, tc.wantValues), tc.caseDesc)
+		assert.True(t, reflect.DeepEqual(args, tc.wantArgs), tc.caseDesc)
+	}
+}
+
+func TestWatchEncode(t *testing.T) {
+	tests := []struct {
+		caseDesc    string
+		giveService Service
+		wantMsg     string
+	}{
+		{
+			caseDesc: "Test Watch Decode",
+			giveService: Service{
+				name: "test",
+				nodes: []Node{
+					{host: "127.0.0.1:80", weight: 10},
+					{host: "127.0.0.1:8080", weight: 20},
+				},
+				entities: []string{
+					"upstream;1",
+					"upstream;2",
+				},
+				args: nil,
+			},
+			wantMsg: `key: event, value: update
+key: service, value: test
+key: entity, value: upstream;1
+key: entity, value: upstream;2
+key: node, value: 127.0.0.1:80
+key: weight, value: 10
+key: node, value: 127.0.0.1:8080
+key: weight, value: 20`,
+		},
+	}
+
+	for _, tc := range tests {
+		msg := watchEncode(tc.giveService)
+		assert.True(t, msg.String() == tc.wantMsg, tc.caseDesc)
+	}
+}
