6280273580ea8d613d11029554708a17c5de2569
gu18168 <gu18168@gmail.com>
2021-07-27 16:00:44 +0800

    feat: separate query and watch messages

--- internal/discoverer/discoverer.go
+++ internal/discoverer/discoverer.go
@@ -1,15 +1,13 @@
 package discoverer
 
 import (
-	"errors"
-	"fmt"
 	"strconv"
 
+	"github.com/api7/apisix-seed/internal/core/comm"
 	"github.com/api7/apisix-seed/internal/utils"
 )
 
 var (
-	queryChecks = [3]string{"event", "entity", "service"}
 	Discoveries = make(map[string]Discover)
 )
 
@@ -18,71 +16,50 @@ type Discover func(disConfig interface{}) Discoverer
 // Discoverer defines the component that interact nacos, consul and so on
 type Discoverer interface {
 	Stop()
-	Query(utils.Message) error
-	Watch() chan utils.Message
+	Query(*comm.Query) error
+	Watch() chan *comm.Watch
 }
 
+// Node defines the upstream machine information
 type Node struct {
 	host   string
 	weight int
 }
 
+// Service defines the service information for discoverer
 type Service struct {
 	name     string
-	nodes    []Node
-	entities []string
+	nodes    []Node   // nodes are the upstream machines of the service
+	entities []string // entities are the upstreams/services/routes that use the service
 	args     map[string]string
 }
 
-// queryDecode check and extract values from the query message
-func queryDecode(msg utils.Message) ([]string, map[string]string, error) {
-	msgLength := len(msg)
-	if msgLength < 3 {
-		return nil, nil, errors.New("incorrect query message format")
-	}
-
-	// Check the required parts of the query message
-	msgValues := make([]string, 3)
-	for i := 0; i < 3; i++ {
-		if msg[i].Key != queryChecks[i] {
-			err := fmt.Sprintf("incorrect query part %d: give %s, require %s", i+1, msg[i].Key, queryChecks[i])
-			return nil, nil, errors.New(err)
-		}
-		msgValues[i] = msg[i].Value
-	}
-
-	// Check query event
-	switch event := msgValues[0]; event {
-	case utils.EventAdd, utils.EventUpdate, utils.EventDelete:
-	default:
-		err := fmt.Sprintf("incorrect query event: %s", event)
-		return nil, nil, errors.New(err)
-	}
-
-	// If additional service discovery arguments exist
-	var msgArgs map[string]string
-	if msgLength > 3 {
-		msgArgs = make(map[string]string, msgLength-3)
-		for i := 3; i < msgLength; i++ {
-			msgArgs[msg[i].Key] = msg[i].Value
-		}
-	}
-
-	return msgValues, msgArgs, nil
-}
-
-// watchEncode encodes a service to the watch message
-func watchEncode(service Service) utils.Message {
-	msg := make(utils.Message, 0, 2+len(service.entities)+2*len(service.nodes))
-	msg.Add("event", utils.EventUpdate)
-	msg.Add("service", service.name)
-	for _, entity := range service.entities {
-		msg.Add("entity", entity)
-	}
-	for _, node := range service.nodes {
+func (s *Service) EncodeNodes() utils.Message {
+	msg := make(utils.Message, 0, 2*len(s.nodes))
+	for _, node := range s.nodes {
 		msg.Add("node", node.host)
 		msg.Add("weight", strconv.Itoa(node.weight))
 	}
-
 	return msg
+}
+
+func (s *Service) EncodeEntities() utils.Message {
+	msg := make(utils.Message, 0, len(s.entities))
+	for _, entity := range s.entities {
+		msg.Add("entity", entity)
+	}
+	return msg
+}
+
+// EncodeWatch encodes a service to the watch message
+func (s *Service) EncodeWatch() (*comm.Watch, error) {
+	header, err := comm.NewWatchHeader([]string{utils.EventUpdate, s.name})
+	if err != nil {
+		return nil, err
+	}
+	entities := s.EncodeEntities()
+	nodes := s.EncodeNodes()
+
+	watch := comm.NewWatch(header, entities, nodes)
+	return &watch, nil
 }
--- internal/discoverer/discoverer_test.go
+++ internal/discoverer/discoverer_test.go
@@ -1,118 +1,12 @@
 package discoverer
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 
-	"github.com/api7/apisix-seed/internal/utils"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQueryCheck(t *testing.T) {
-	tests := []struct {
-		caseDesc string
-		giveMsg  [][2]string
-		wantErr  error
-	}{
-		{
-			caseDesc: "Test Wrong Format: empty message",
-			giveMsg:  [][2]string{},
-			wantErr:  fmt.Errorf("incorrect query message format"),
-		},
-		{
-			caseDesc: "Test Incorrect Part: incorrect event",
-			giveMsg: [][2]string{
-				{"action", "add"},
-				{"entity", "upstream;1"},
-				{"service", "test"},
-			},
-			wantErr: fmt.Errorf("incorrect query part 1: give action, require event"),
-		},
-		{
-			caseDesc: "Test Incorrect Part: incorrect entity",
-			giveMsg: [][2]string{
-				{"event", "add"},
-				{"entities", "upstream;1"},
-				{"service", "test"},
-			},
-			wantErr: fmt.Errorf("incorrect query part 2: give entities, require entity"),
-		},
-		{
-			caseDesc: "Test Incorrect Part: incorrect service",
-			giveMsg: [][2]string{
-				{"event", "add"},
-				{"entity", "upstream;1"},
-				{"services", "test"},
-			},
-			wantErr: fmt.Errorf("incorrect query part 3: give services, require service"),
-		},
-		{
-			caseDesc: "Test Incorrect Event",
-			giveMsg: [][2]string{
-				{"event", "remove"},
-				{"entity", "upstream;1"},
-				{"service", "test"},
-			},
-			wantErr: fmt.Errorf("incorrect query event: remove"),
-		},
-	}
-
-	for _, tc := range tests {
-		msg := make(utils.Message, 0, len(tc.giveMsg))
-		for _, pair := range tc.giveMsg {
-			msg.Add(pair[0], pair[1])
-		}
-		_, _, err := queryDecode(msg)
-		assert.True(t, tc.wantErr.Error() == err.Error(), tc.caseDesc)
-	}
-}
-
-func TestQueryEncode(t *testing.T) {
-	tests := []struct {
-		caseDesc   string
-		giveMsg    [][2]string
-		wantValues []string
-		wantArgs   map[string]string
-	}{
-		{
-			caseDesc: "Test Query Encode without extra arguments",
-			giveMsg: [][2]string{
-				{"event", "add"},
-				{"entity", "upstream;1"},
-				{"service", "test"},
-			},
-			wantValues: []string{"add", "upstream;1", "test"},
-			wantArgs:   nil,
-		},
-		{
-			caseDesc: "Test Query Encode with arguments",
-			giveMsg: [][2]string{
-				{"event", "add"},
-				{"entity", "upstream;1"},
-				{"service", "test"},
-				{"namespace_id", "test_ns"},
-			},
-			wantValues: []string{"add", "upstream;1", "test"},
-			wantArgs: map[string]string{
-				"namespace_id": "test_ns",
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		msg := make(utils.Message, 0, len(tc.giveMsg))
-		for _, pair := range tc.giveMsg {
-			msg.Add(pair[0], pair[1])
-		}
-		values, args, err := queryDecode(msg)
-		assert.Nil(t, err)
-		assert.True(t, reflect.DeepEqual(values, tc.wantValues), tc.caseDesc)
-		assert.True(t, reflect.DeepEqual(args, tc.wantArgs), tc.caseDesc)
-	}
-}
-
-func TestWatchEncode(t *testing.T) {
+func TestServiceEncodeWatch(t *testing.T) {
 	tests := []struct {
 		caseDesc    string
 		giveService Service
@@ -144,7 +38,8 @@ key: weight, value: 20`,
 	}
 
 	for _, tc := range tests {
-		msg := watchEncode(tc.giveService)
-		assert.True(t, msg.String() == tc.wantMsg, tc.caseDesc)
+		watch, err := tc.giveService.EncodeWatch()
+		assert.Nil(t, err)
+		assert.True(t, watch.String() == tc.wantMsg, tc.caseDesc)
 	}
 }
