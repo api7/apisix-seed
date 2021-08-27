c798b24a66e81a85b27a2db7bfe314642572340d
gu18168 <gu18168@gmail.com>
2021-08-27 20:35:11 +0800

    fix: test service encode watch

--- internal/discoverer/discoverer_test.go
+++ internal/discoverer/discoverer_test.go
@@ -10,7 +10,7 @@ func TestServiceEncodeWatch(t *testing.T) {
 	tests := []struct {
 		caseDesc    string
 		giveService Service
-		wantMsg     string
+		wantMsg     []string
 	}{
 		{
 			caseDesc: "Test Watch Decode",
@@ -26,20 +26,33 @@ func TestServiceEncodeWatch(t *testing.T) {
 				},
 				args: nil,
 			},
-			wantMsg: `key: event, value: update
+			wantMsg: []string{`key: event, value: update
 key: service, value: test
 key: entity, value: upstream;1
 key: entity, value: upstream;2
 key: node, value: 127.0.0.1:80
 key: weight, value: 10
 key: node, value: 127.0.0.1:8080
-key: weight, value: 20`,
-		},
+key: weight, value: 20`, `key: event, value: update
+key: service, value: test
+key: entity, value: upstream;2
+key: entity, value: upstream;1
+key: node, value: 127.0.0.1:80
+key: weight, value: 10
+key: node, value: 127.0.0.1:8080
+key: weight, value: 20`}},
 	}
 
 	for _, tc := range tests {
 		watch, err := tc.giveService.EncodeWatch()
 		assert.Nil(t, err)
-		assert.True(t, watch.String() == tc.wantMsg, tc.caseDesc)
+		ret := false
+		for _, msg := range tc.wantMsg {
+			if watch.String() == msg {
+				ret = true
+				break
+			}
+		}
+		assert.True(t, ret, tc.caseDesc)
 	}
 }
