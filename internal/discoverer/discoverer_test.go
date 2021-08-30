2985d32789442e485d69a2fbd11c8afc11fd7d4a
gu18168 <gu18168@gmail.com>
2021-08-30 13:10:04 +0800

    chore: sort service watch's entities

--- internal/discoverer/discoverer_test.go
+++ internal/discoverer/discoverer_test.go
@@ -10,7 +10,7 @@ func TestServiceEncodeWatch(t *testing.T) {
 	tests := []struct {
 		caseDesc    string
 		giveService Service
-		wantMsg     []string
+		wantMsg     string
 	}{
 		{
 			caseDesc: "Test Watch Decode",
@@ -21,38 +21,24 @@ func TestServiceEncodeWatch(t *testing.T) {
 					{host: "127.0.0.1:8080", weight: 20},
 				},
 				entities: map[string]struct{}{
-					"upstream;1": {},
 					"upstream;2": {},
+					"upstream;1": {},
 				},
 				args: nil,
 			},
-			wantMsg: []string{`key: event, value: update
+			wantMsg: `key: event, value: update
 key: service, value: test
 key: entity, value: upstream;1
 key: entity, value: upstream;2
 key: node, value: 127.0.0.1:80
 key: weight, value: 10
 key: node, value: 127.0.0.1:8080
-key: weight, value: 20`, `key: event, value: update
-key: service, value: test
-key: entity, value: upstream;2
-key: entity, value: upstream;1
-key: node, value: 127.0.0.1:80
-key: weight, value: 10
-key: node, value: 127.0.0.1:8080
-key: weight, value: 20`}},
+key: weight, value: 20`},
 	}
 
 	for _, tc := range tests {
 		watch, err := tc.giveService.EncodeWatch()
 		assert.Nil(t, err)
-		ret := false
-		for _, msg := range tc.wantMsg {
-			if watch.String() == msg {
-				ret = true
-				break
-			}
-		}
-		assert.True(t, ret, tc.caseDesc)
+		assert.True(t, watch.String() == tc.wantMsg, tc.caseDesc)
 	}
 }
