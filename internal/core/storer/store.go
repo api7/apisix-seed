05461a67c807bfaa46fe28b2822d09c9331add20
gu18168 <gu18168@gmail.com>
2021-09-09 16:23:38 +0800

    feat: add Storer

---
+++ internal/core/storer/store.go
@@ -0,0 +1,152 @@
+package storer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/api7/apisix-seed/internal/core/entity"
+	"github.com/api7/apisix-seed/internal/utils"
+)
+
+type Interface interface {
+	List(context.Context, string) (utils.Message, error)
+	Update(context.Context, string, string) error
+	Watch(context.Context, string) <-chan *Watch
+}
+
+type GenericStoreOption struct {
+	BasePath string
+	ObjType  reflect.Type
+}
+
+type GenericStore struct {
+	Stg Interface
+
+	cache sync.Map
+	opt   GenericStoreOption
+
+	cancel context.CancelFunc
+}
+
+func NewGenericStore(opt GenericStoreOption, stg Interface) (*GenericStore, error) {
+	if opt.BasePath == "" {
+		return nil, fmt.Errorf("base path can not be empty")
+	}
+	if opt.ObjType == nil {
+		return nil, fmt.Errorf("object type can not be nil")
+	}
+
+	if opt.ObjType.Kind() == reflect.Ptr {
+		opt.ObjType = opt.ObjType.Elem()
+	}
+	if opt.ObjType.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("object type is invalid")
+	}
+	s := &GenericStore{
+		Stg: stg,
+		opt: opt,
+	}
+
+	return s, nil
+}
+
+func (s *GenericStore) List() ([]interface{}, error) {
+	lc, lcancel := context.WithTimeout(context.TODO(), 5*time.Second)
+	defer lcancel()
+	ret, err := s.Stg.List(lc, s.opt.BasePath)
+	if err != nil {
+		return nil, err
+	}
+
+	objPtrs := make([]interface{}, len(ret))
+	for i := range ret {
+		objPtr, err := s.StringToObjPtr(ret[i].Value, ret[i].Key)
+		if err != nil {
+			return nil, err
+		}
+
+		objPtrs[i] = objPtr
+	}
+
+	return objPtrs, nil
+}
+
+func (s *GenericStore) Watch() <-chan *Watch {
+	c, cancel := context.WithCancel(context.TODO())
+	s.cancel = cancel
+
+	ch := s.Stg.Watch(c, s.opt.BasePath)
+
+	return ch
+}
+
+func (s *GenericStore) Unwatch() {
+	s.cancel()
+}
+
+func (s *GenericStore) UpdateNodes(ctx context.Context, key string, nodes []*entity.Node) error {
+	if key == "" {
+		return fmt.Errorf("key is required")
+	}
+
+	storedObj, ok := s.cache.Load(key)
+	if !ok {
+		return fmt.Errorf("key: %s is not found", key)
+	}
+
+	// Update Nodes Information
+	if setter, ok := storedObj.(entity.NodesSetter); ok {
+		setter.SetNodes(nodes)
+	} else {
+		return fmt.Errorf("obj can't set nodes")
+	}
+
+	if setter, ok := storedObj.(entity.BaseInfoSetter); ok {
+		info := setter.GetBaseInfo()
+		info.Updating(info)
+	}
+
+	bs, err := json.Marshal(storedObj)
+	if err != nil {
+		return fmt.Errorf("json marshal failed: %s", err)
+	}
+	if err = s.Stg.Update(ctx, key, string(bs)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *GenericStore) Store(key string, objPtr interface{}) (interface{}, bool) {
+	return s.cache.LoadOrStore(s.key(key), objPtr)
+}
+
+func (s *GenericStore) Delete(key string) (interface{}, bool) {
+	return s.cache.LoadAndDelete(key)
+}
+
+func (s *GenericStore) StringToObjPtr(str, key string) (interface{}, error) {
+	key = s.key(key)
+
+	objPtr := reflect.New(s.opt.ObjType)
+	ret := objPtr.Interface()
+	err := json.Unmarshal([]byte(str), ret)
+	if err != nil {
+		return nil, fmt.Errorf("json unmarshal failed\n\tRelated Key:\t\t%s\n\tError Description:\t%s", key, err)
+	}
+
+	if setter, ok := ret.(entity.BaseInfoSetter); ok {
+		info := setter.GetBaseInfo()
+		info.KeyCompat(key)
+	}
+
+	return ret, nil
+}
+
+func (s *GenericStore) key(key string) string {
+	return key[len(s.opt.BasePath)+1:]
+}
---
+++ internal/core/storer/store_test.go
@@ -0,0 +1,327 @@
+package storer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/api7/apisix-seed/internal/core/entity"
+	"github.com/api7/apisix-seed/internal/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestNewGenericStore(t *testing.T) {
+	tests := []struct {
+		caseDesc  string
+		giveOpt   GenericStoreOption
+		wantStore *GenericStore
+		wantErr   error
+	}{
+		{
+			caseDesc: "Normal Case",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(GenericStoreOption{}),
+			},
+			wantStore: &GenericStore{
+				Stg: nil,
+				opt: GenericStoreOption{
+					BasePath: "test",
+					ObjType:  reflect.TypeOf(GenericStoreOption{}),
+				},
+			},
+		},
+		{
+			caseDesc: "No BasePath",
+			giveOpt: GenericStoreOption{
+				BasePath: "",
+				ObjType:  reflect.TypeOf(GenericStoreOption{}),
+			},
+			wantErr: fmt.Errorf("base path can not be empty"),
+		},
+		{
+			caseDesc: "No object type",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  nil,
+			},
+			wantErr: fmt.Errorf("object type can not be nil"),
+		},
+		{
+			caseDesc: "Invalid object type",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(""),
+			},
+			wantErr: fmt.Errorf("object type is invalid"),
+		},
+	}
+
+	for _, tc := range tests {
+		s, err := NewGenericStore(tc.giveOpt, nil)
+		assert.Equal(t, tc.wantErr, err, tc.caseDesc)
+		if err != nil {
+			continue
+		}
+		assert.Equal(t, tc.wantStore.Stg, s.Stg, tc.caseDesc)
+		flag := reflect.DeepEqual(&tc.wantStore.cache, &s.cache)
+		assert.True(t, flag, tc.caseDesc)
+		assert.Equal(t, tc.wantStore.opt.BasePath, s.opt.BasePath, tc.caseDesc)
+		assert.Equal(t, tc.wantStore.opt.ObjType, s.opt.ObjType, tc.caseDesc)
+	}
+}
+
+type TestStruct struct {
+	entity.BaseInfo
+	Field1 string
+	Field2 string
+}
+
+func TestList(t *testing.T) {
+	tests := []struct {
+		caseDesc    string
+		giveOpt     GenericStoreOption
+		giveListErr error
+		giveListRet utils.Message
+		wantErr     error
+		wantCache   map[string]interface{}
+	}{
+		{
+			caseDesc: "sanity",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestStruct{}),
+			},
+			giveListRet: utils.Message{
+				{
+					Key:   "test/demo1-f1",
+					Value: `{"Field1":"demo1-f1", "Field2":"demo1-f2"}`,
+				},
+				{
+					Key:   "test/demo2-f1",
+					Value: `{"Field1":"demo2-f1", "Field2":"demo2-f2"}`,
+				},
+			},
+			wantCache: map[string]interface{}{
+				"demo1-f1": &TestStruct{
+					BaseInfo: entity.BaseInfo{ID: "demo1-f1"},
+					Field1:   "demo1-f1",
+					Field2:   "demo1-f2",
+				},
+				"demo2-f1": &TestStruct{
+					BaseInfo: entity.BaseInfo{ID: "demo2-f1"},
+					Field1:   "demo2-f1",
+					Field2:   "demo2-f2",
+				},
+			},
+		},
+		{
+			caseDesc: "list error",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestStruct{}),
+			},
+			giveListErr: fmt.Errorf("list error"),
+			wantErr:     fmt.Errorf("list error"),
+		},
+		{
+			caseDesc: "json error",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestStruct{}),
+			},
+			giveListRet: utils.Message{
+				{
+					Key:   "test/demo1-f1",
+					Value: `{"Field1","demo1-f1", "Field2":"demo1-f2"}`,
+				},
+			},
+			wantErr: fmt.Errorf("json unmarshal failed\n\tRelated Key:\t\tdemo1-f1\n\tError Description:\t" +
+				"invalid character ',' after object key"),
+		},
+	}
+
+	for _, tc := range tests {
+		mStg := &MockInterface{}
+		mStg.On("List", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+			assert.Equal(t, tc.giveOpt.BasePath, args[0], tc.caseDesc)
+		}).Return(tc.giveListRet, tc.giveListErr)
+
+		store, err := NewGenericStore(tc.giveOpt, mStg)
+		assert.Nil(t, err, tc.caseDesc)
+
+		_, err = store.List()
+		if err != nil {
+			assert.Equal(t, tc.wantErr.Error(), err.Error(), tc.caseDesc)
+			continue
+		}
+
+		store.cache.Range(func(key, value interface{}) bool {
+			assert.Equal(t, tc.wantCache[key.(string)], value, tc.caseDesc)
+			return true
+		})
+	}
+}
+
+func TestWatch(t *testing.T) {
+	tests := []struct {
+		caseDesc    string
+		giveOpt     GenericStoreOption
+		giveWatchCh chan *Watch
+		giveWatch   *Watch
+	}{
+		{
+			caseDesc: "sanity",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestStruct{}),
+			},
+			giveWatchCh: make(chan *Watch, 1),
+			giveWatch: &Watch{
+				Events: []Event{
+					{
+						utils.Message{
+							{Key: "event", Value: utils.EventDelete},
+							{Key: "key", Value: "test/demo1-f1"},
+							{Key: "value", Value: ""},
+						},
+					},
+					{
+						utils.Message{
+							{Key: "event", Value: utils.EventAdd},
+							{Key: "key", Value: "test/demo3-f1"},
+							{Key: "value", Value: `{"Field1":"demo3-f1", "Field2":"demo3-f2"}`},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		mStg := &MockInterface{}
+		mStg.On("Watch", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+			assert.Equal(t, tc.giveOpt.BasePath, args[0], tc.caseDesc)
+		}).Return(tc.giveWatchCh)
+
+		store, err := NewGenericStore(tc.giveOpt, mStg)
+		assert.Nil(t, err, tc.caseDesc)
+		tc.giveWatchCh <- tc.giveWatch
+		watch := <-store.Watch()
+		assert.Equal(t, tc.giveWatch, watch, tc.caseDesc)
+	}
+}
+
+type TestNodes struct {
+	entity.BaseInfo
+	Nodes []*entity.Node
+}
+
+func (t *TestNodes) SetNodes(nodes []*entity.Node) {
+	t.Nodes = nodes
+}
+
+func TestUpdate(t *testing.T) {
+	tests := []struct {
+		caseDesc  string
+		giveOpt   GenericStoreOption
+		giveCache map[string]interface{}
+		giveErr   error
+		giveKey   string
+		giveNodes []*entity.Node
+		wantErr   error
+	}{
+		{
+			caseDesc: "sanity",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestNodes{}),
+			},
+			giveCache: map[string]interface{}{
+				"test/test1": &TestNodes{},
+			},
+			giveKey: "test1",
+			giveNodes: []*entity.Node{
+				{Host: "test.com", Weight: 10},
+			},
+		},
+		{
+			caseDesc: "not found",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestNodes{}),
+			},
+			giveCache: map[string]interface{}{
+				"test/test1": &TestNodes{},
+			},
+			giveKey: "test2",
+			wantErr: fmt.Errorf("key: test2 is not found"),
+		},
+		{
+			caseDesc: "not NodesSetter",
+			giveOpt: GenericStoreOption{
+				BasePath: "test",
+				ObjType:  reflect.TypeOf(TestStruct{}),
+			},
+			giveCache: map[string]interface{}{
+				"test/test1": &TestStruct{},
+			},
+			giveKey: "test1",
+			wantErr: fmt.Errorf("obj can't set nodes"),
+		},
+	}
+
+	for _, tc := range tests {
+		mStg := &MockInterface{}
+		mStg.On("Update", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+			assert.Equal(t, tc.giveKey, args[0], tc.caseDesc)
+			input := TestNodes{}
+			err := json.Unmarshal([]byte(args[1].(string)), &input)
+			assert.Nil(t, err)
+			assert.Equal(t, len(tc.giveNodes), len(input.Nodes), tc.caseDesc)
+			for i := range tc.giveNodes {
+				assert.Equal(t, *tc.giveNodes[i], *input.Nodes[i], tc.caseDesc)
+			}
+			assert.NotEqual(t, 0, input.UpdateTime, tc.caseDesc)
+		}).Return(tc.giveErr)
+
+		store, err := NewGenericStore(tc.giveOpt, mStg)
+		assert.Nil(t, err, tc.caseDesc)
+
+		for k, v := range tc.giveCache {
+			store.Store(k, v)
+		}
+
+		err = store.UpdateNodes(context.TODO(), tc.giveKey, tc.giveNodes)
+		if err != nil {
+			assert.Equal(t, tc.wantErr, err, tc.caseDesc)
+			continue
+		}
+		ret, ok := store.cache.Load(tc.giveKey)
+		assert.True(t, ok, tc.caseDesc)
+		retTn, ok := ret.(*TestNodes)
+		assert.True(t, ok, tc.caseDesc)
+
+		assert.Equal(t, len(tc.giveNodes), len(retTn.Nodes), tc.caseDesc)
+		for i := range tc.giveNodes {
+			assert.Equal(t, *tc.giveNodes[i], *retTn.Nodes[i], tc.caseDesc)
+		}
+	}
+}
+
+func TestStringToObjPtr(t *testing.T) {
+	s, err := NewGenericStore(GenericStoreOption{
+		BasePath: "test",
+		ObjType:  reflect.TypeOf(entity.Upstream{}),
+	}, nil)
+	assert.Nil(t, err)
+	rawID, id := "test/1", "1"
+	argStr := `{"namespace_id":"dev", "group_name":"test"}`
+	argInterface, err := s.StringToObjPtr(argStr, rawID)
+	assert.Nil(t, err)
+	arg := argInterface.(*entity.Upstream)
+	assert.Equal(t, id, arg.ID)
+}
