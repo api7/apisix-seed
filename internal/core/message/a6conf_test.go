2f9584cb3c303d142f8ad233cb716ee0bdba9aa9
soulbird <zhaothree@gmail.com>
2022-11-21 20:23:02 +0800

    add test case

--- internal/core/message/a6conf_test.go
+++ internal/core/message/a6conf_test.go
@@ -84,7 +84,7 @@ func TestInject_Routes(t *testing.T) {
 	assert.Len(t, a6.GetUpstream().Nodes, 2)
 }
 
-func TestMarshal(t *testing.T) {
+func TestMarshal_Routes(t *testing.T) {
 	givenA6Str := `{
     "status": 1,
     "id": "3",
@@ -149,6 +149,214 @@ func TestMarshal(t *testing.T) {
 	assert.Nil(t, err, caseDesc)
 
 	assert.JSONEq(t, wantA6Str, string(ss))
+}
+
+func TestNewA6Conf_Services(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		value string
+		err   string
+	}{
+		{
+			desc: "normal",
+			value: `{
+    "enable_websocket": false,
+    "upstream": {
+        "discovery_type": "nacos",
+        "service_name": "APISIX-NACOS",
+        "discovery_args": {
+            "group_name": "DEFAULT_GROUP"
+        }
+    }
+}`,
+		},
+		{
+			desc: "error conf",
+			value: `{
+    "enable_websocket": false
+    "upstream": {
+        "discovery_type": "nacos",
+        "service_name": "APISIX-NACOS",
+        "discovery_args": {
+            "group_name": "DEFAULT_GROUP"
+        }
+    }
+}`,
+			err: `invalid character '"' after object key:value pair`,
+		},
+	}
+
+	for _, v := range testCases {
+		a6, err := NewA6Conf([]byte(v.value), A6ServicesConf)
+		if v.err != "" {
+			assert.Equal(t, v.err, err.Error(), v.desc)
+		} else {
+			assert.Nil(t, err, v.desc)
+			assert.Equal(t, "nacos", a6.GetUpstream().DiscoveryType)
+			assert.Equal(t, "APISIX-NACOS", a6.GetUpstream().ServiceName)
+		}
+
+	}
+}
+
+func TestInject_Services(t *testing.T) {
+	givenA6Str := `{
+    "enable_websocket": false,
+    "upstream": {
+        "discovery_type": "nacos",
+        "service_name": "APISIX-NACOS",
+        "discovery_args": {
+            "group_name": "DEFAULT_GROUP"
+        }
+    }
+}`
+	nodes := []*Node{
+		{
+			Host:   "192.168.1.1",
+			Port:   80,
+			Weight: 1,
+		},
+		{
+			Host:   "192.168.1.2",
+			Port:   80,
+			Weight: 1,
+		},
+	}
+	caseDesc := "sanity"
+	a6, err := NewA6Conf([]byte(givenA6Str), A6ServicesConf)
+	assert.Nil(t, err, caseDesc)
+	a6.Inject(nodes)
+	assert.Len(t, a6.GetUpstream().Nodes, 2)
+}
+
+func TestMarshal_Services(t *testing.T) {
+	givenA6Str := `{
+    "enable_websocket": false,
+    "upstream": {
+        "scheme": "http",
+        "pass_host": "pass",
+        "type": "roundrobin",
+        "hash_on": "vars",
+        "discovery_type": "nacos",
+        "service_name": "APISIX-NACOS",
+        "discovery_args": {
+            "group_name": "DEFAULT_GROUP"
+        }
+    },
+    "create_time": 1648871506,
+    "update_time": 1648871506
+}`
+	nodes := []*Node{
+		{Host: "192.168.1.1", Port: 80, Weight: 1},
+		{Host: "192.168.1.2", Port: 80, Weight: 1},
+	}
+
+	wantA6Str := `{
+    "enable_websocket": false,
+    "upstream": {
+        "scheme": "http",
+        "pass_host": "pass",
+        "type": "roundrobin",
+        "hash_on": "vars",
+        "_discovery_type": "nacos",
+        "_service_name": "APISIX-NACOS",
+        "discovery_args": {
+            "group_name": "DEFAULT_GROUP"
+        },
+        "nodes": [
+            {
+                "host": "192.168.1.1",
+                "port": 80,
+                "weight": 1
+            },
+            {
+                "host": "192.168.1.2",
+                "port": 80,
+                "weight": 1
+            }
+        ]
+    },
+    "create_time": 1648871506,
+    "update_time": 1648871506
+}`
+	caseDesc := "sanity"
+	a6, err := NewA6Conf([]byte(givenA6Str), A6RoutesConf)
+	assert.Nil(t, err, caseDesc)
+
+	a6.Inject(&nodes)
+	ss, err := a6.Marshal()
+	assert.Nil(t, err, caseDesc)
+
+	assert.JSONEq(t, wantA6Str, string(ss))
+}
+
+func TestNewA6Conf_Upstreams(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		value string
+		err   string
+	}{
+		{
+			desc: "normal",
+			value: `{
+    "discovery_type": "nacos",
+	"service_name": "APISIX-NACOS",
+	"discovery_args": {
+		"group_name": "DEFAULT_GROUP"
+	}
+}`,
+		},
+		{
+			desc: "error conf",
+			value: `{
+    "discovery_type": "nacos"
+	"service_name": "APISIX-NACOS",
+	"discovery_args": {
+		"group_name": "DEFAULT_GROUP"
+	}
+}`,
+			err: `invalid character '"' after object key:value pair`,
+		},
+	}
+
+	for _, v := range testCases {
+		a6, err := NewA6Conf([]byte(v.value), A6UpstreamsConf)
+		if v.err != "" {
+			assert.Equal(t, v.err, err.Error(), v.desc)
+		} else {
+			assert.Nil(t, err, v.desc)
+			assert.Equal(t, "nacos", a6.GetUpstream().DiscoveryType)
+			assert.Equal(t, "APISIX-NACOS", a6.GetUpstream().ServiceName)
+		}
+
+	}
+}
+
+func TestInject_Upstreams(t *testing.T) {
+	givenA6Str := `{
+    "discovery_type": "nacos",
+	"service_name": "APISIX-NACOS",
+	"discovery_args": {
+		"group_name": "DEFAULT_GROUP"
+	}
+}`
+	nodes := []*Node{
+		{
+			Host:   "192.168.1.1",
+			Port:   80,
+			Weight: 1,
+		},
+		{
+			Host:   "192.168.1.2",
+			Port:   80,
+			Weight: 1,
+		},
+	}
+	caseDesc := "sanity"
+	a6, err := NewA6Conf([]byte(givenA6Str), A6UpstreamsConf)
+	assert.Nil(t, err, caseDesc)
+	a6.Inject(nodes)
+	assert.Len(t, a6.GetUpstream().Nodes, 2)
 }
 
 func TestMarshal_Upstreams(t *testing.T) {
